709c7547d82bc76f4e77cf750097f9d3ce7037a9
Radek Pieczątkiewicz <radek.pieczatkiewicz@brightinventions.pl>
2024-09-24 21:44:32 +0200

    Baza with simple servo 360 rotation

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,27 @@
 static const String BT_DEVICE_NAME = "ESP32_Baza1";
 static const int UART_VERSION = 2; // Using UART2
 static const int servo360Pin = 13; // motor pin
-static const int rxPin = 16; // reveive pin
-static const int txPin = 17; // transmit pin
+static const int rxPin = 16;       // reveive pin
+static const int txPin = 17;       // transmit pin
+
+int neutralPWM = 1500;
+int speedPWM = 300;
 
 BluetoothSerial SerialBT;
 HardwareSerial DaisyChain(UART_VERSION);
-//Servo servo360;
+Servo servo360;
 
-void handleCommand(int command);
+void handleBTCommand(int command);
+void rotateForward();
+void rotateBack();
+void rotate(int direction, uint32_t ms);
 
 void setup()
 {
   Serial.begin(115200);
   SerialBT.begin(BT_DEVICE_NAME);
   DaisyChain.begin(9600, SERIAL_8N1, rxPin, txPin);
-  //servo360.attach(servo360Pin);
+  servo360.attach(servo360Pin);
   Serial.println("Baza setup completed");
 }
 
@@ -38,26 +44,52 @@ void loop()
   if (SerialBT.available())
   {
     int serialBTRead = SerialBT.read();
-    handleCommand(serialBTRead);
+    handleBTCommand(serialBTRead);
   }
   delay(20);
 }
 
-void handleCommand(int command)
+void handleBTCommand(int command)
 {
   Serial.print("Handling command: ");
   Serial.println(command);
   switch (command)
   {
+
+  case '3':
+    rotateBack();
+    break;
+
+  case '2':
+    rotateForward();
+    break;
+
   case '1':
-    DaisyChain.print(command);
+    DaisyChain.println("Hello jedyneczka");
     break;
 
   case '0':
-    DaisyChain.print(command);
+    DaisyChain.println("Siema zeroweczka");
     break;
 
   default:
     break;
   }
+}
+
+void rotateForward()
+{
+  rotate(1, 5000);
+}
+
+void rotateBack()
+{
+  rotate(-1, 5000);
+}
+
+void rotate(int direction, uint32_t ms)
+{
+  servo360.writeMicroseconds(neutralPWM + direction * speedPWM);
+  delay(ms);
+  servo360.writeMicroseconds(neutralPWM);
 }
